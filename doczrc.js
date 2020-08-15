7f620acbbe40d7d6f20931b55b6d9c76fa407cbb
Silviu Avram <silviu.avram91@gmail.com>
2020-08-15 19:59:51 +0300

    chore: copy the docsite contents

---
+++ doczrc.js
@@ -0,0 +1,28 @@
+export default {
+  port: 6006,
+  src: './src/',
+  menu: [
+    'Home',
+    'Components',
+    {name: 'Hooks', menu: ['useCombobox', 'useMultipleSelection', 'useSelect']},
+    'Tests',
+  ],
+  themeConfig: {
+    initialColorMode: 'light',
+    showDarkModeSwitch: false,
+    breakpoints: ['920px'],
+    logo: {
+      src: '/public/logo/downshift.svg',
+      margin: 'auto',
+      width: 128,
+    },
+    styles: {
+      Container: {
+        width: ['100%', '75%'],
+      },
+      inlineCode: {
+        bg: 'blockquote.bg',
+      },
+    },
+  },
+}
